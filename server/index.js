aca92823b9e4dc65f223248aa477178cbbdee8b4
randhirsingh77 <rs6201849307@gmail.com>
2025-09-28 01:05:09 +0530

    deploying changes

--- server/index.js
+++ server/index.js
@@ -33,8 +33,8 @@ app.use("/api/contact", contactRoutes);
 // Serve frontend (Vite build)
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
